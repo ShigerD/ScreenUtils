e76890e6e3cf5941127f07d8739135054e60f00c
yangcanhu <canhu.yang@imotorinfo.com>
2017-12-08 14:54:17 +0800

    rejust the ui and add appInfo

---
+++ app/src/main/java/com/android/tiger/tools/BaseActivity.java
@@ -0,0 +1,31 @@
+package com.android.tiger.tools;
+
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class BaseActivity extends Activity {
+    private static final String TAG = "BaseActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //沉浸式状态栏
+        Window window = getWindow();
+        Log.d(TAG,"Build.VERSION.SDK_INT===="+ Build.VERSION.SDK_INT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);// total alpha
+        }
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_OVERSCAN);// total alpha
+//            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//half alpha
+        }
+    }
+}
